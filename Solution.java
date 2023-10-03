56704d1e38ab9fa2204e8b2775573ca027e73296
shubhamRaje98 <shubhamshejaval@gmail.com>
2023-10-03 05:38:46 +0530

    initial commit

---
+++ Solution.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        /*
+        try {
+            System.setIn(new FileInputStream("input.txt"));
+            System.setOut(new PrintStream(new FileOutputStream("output.txt")));
+        } catch (Exception e) {
+            System.err.println("Error");
+        }
+*/
+
+        System.out.println("Executing java file");
+        System.out.println(new Date());
+    }
+
+    
+}
